a2519b8f1299ab5b44b1984c0feac86700fa7f00
esy20003 <esy20003@gmail.com>
2023-06-29 18:15:19 +0900

    카카오톡 로그인 성공

--- ticketProject/procedure.sql
+++ ticketProject/procedure.sql
@@ -1,11 +1,11 @@
 
 CREATE OR REPLACE PROCEDURE getMember(
     p_id IN member.id%type,
-    p_curvar OUT SYS_REFCURSOR
+    p_cur OUT SYS_REFCURSOR
 )
 IS
 BEGIN
-    OPEN p_curvar For SELECT * FROM member WHERE id=p_id;
+    OPEN p_cur FOR SELECT * FROM member WHERE id=p_id;
 END;
 
 
@@ -15,3 +15,48 @@ commit;
 select * from member;
 alter table member add provider varchar2(100);
 
+--카카오로그인
+CREATE OR REPLACE PROCEDURE joinKakao(
+    p_id IN member.id%TYPE,
+    p_name IN member.name%TYPE,
+    p_email IN member.email%TYPE,
+    p_provider IN member.provider%TYPE
+)
+IS
+BEGIN
+    INSERT INTO member( mseq, id, name, email, provider)
+    VALUES( member_mseq.nextVal, p_id , p_name , p_email , p_provider);
+    COMMIT;
+END;
+
+commit;
+
+
+
+
+
+update member set name = '홍낄동' where mseq = 29;
+commit;
+select * from member;
+alter table member modify id varchar2(30);
+alter table member modify pwd varchar2(30) null;
+
+alter table member modify name varchar2(30) null;
+alter table member modify nickname varchar2(30) null;
+alter table member modify phone varchar2(30) null;
+alter table member modify birth date null;
+alter table member modify zip_num varchar2(30) null;
+alter table member modify address1 varchar2(100) null; -- null 과 not null 을 변경하면서 칼럼의 바이트 를 적게 설정할 수 없다
+alter table member modify address2 varchar2(30) null;
+alter table member add address3 varchar2(50) null;
+
+
+
+commit;
+
+
+select * from all_sequences where sequence = 'member_mseq';
+
+SELECT *
+  FROM all_sequences
+ WHERE sequence_name = 'member_mseq';
--- ticketProject/src/main/java/com/ticket/t1/controller/MemberController.java
+++ ticketProject/src/main/java/com/ticket/t1/controller/MemberController.java
@@ -2,10 +2,6 @@ package com.ticket.t1.controller;
 
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -18,27 +14,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.net.ssl.HttpsURLConnection;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-
 import org.springframework.context.annotation.Profile;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.ticket.t1.dto.MemberVO;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
@@ -46,7 +33,6 @@ import com.ticket.t1.dto.KakaoProfile;
 import com.ticket.t1.dto.KakaoProfile.KakaoAccount;
 import com.ticket.t1.dto.MemberVO;
 import com.ticket.t1.dto.OAuthToken;
-
 import com.ticket.t1.service.MemberService;
 
 @Controller
@@ -119,10 +105,84 @@ public class MemberController {
 	public @ResponseBody String kakaostart() { // 비동기식 통신
 		// 카카오 로그인 rest api key 를 사용
 		String a = "<script type='text/javascript'>" + "location.href='https://kauth.kakao.com/oauth/authorize?"
-				+ "client_id=4fde1cbde4d82ed7b25bd2a750d98d3c&" + "redirect_uri=http://localhost:8070/kakaoLogin&"
+				+ "client_id=bab305fbb8a02d4bd91ccc64d508aabe&" + "redirect_uri=http://localhost:8070/kakaoLogin&"
 				+ "response_type=code';" + "</script>";
 		return a; // 아무거나 리턴시켜도 되나?
 	}
+	
+	
+//	@RequestMapping("/kakaoLogin")
+//	public String loginKakao(HttpServletRequest request) throws UnsupportedEncodingException, IOException {
+//
+//		String code = request.getParameter("code");
+//		String endpoint = "https://kauth.kakao.com/oauth/token";
+//		URL url = new URL(endpoint); // import java.net.URL;
+//		String bodyData = "grant_type=authorization_code&";
+//		bodyData += "client_id=bab305fbb8a02d4bd91ccc64d508aabe&";
+//		bodyData += "redirect_uri=http://localhost:8070/kakaoLogin&";
+//		bodyData += "code=" + code;
+//		
+//		HttpURLConnection conn = (HttpURLConnection) url.openConnection(); // import java.net.HttpURLConnection;
+//		conn.setRequestMethod("POST");
+//		conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+//		conn.setDoOutput(true);
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream(), "UTF-8"));
+//		bw.write(bodyData);
+//		bw.flush();
+//		BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+//		String input = "";
+//		StringBuilder sb = new StringBuilder(); // 조각난 String 을 조립하기위한 객체
+//		while ((input = br.readLine()) != null) {
+//			sb.append(input);
+//			System.out.println(input); // 수신된 토큰을 콘솔에 출력합니다
+//		}
+//		Gson gson = new Gson();
+//		OAuthToken oAuthToken = gson.fromJson(sb.toString(), OAuthToken.class);
+//		String endpoint2 = "https://kapi.kakao.com/v2/user/me";
+//		URL url2 = new URL(endpoint2);
+//		
+//		HttpsURLConnection conn2 = (HttpsURLConnection) url2.openConnection();
+//		conn2.setRequestProperty("Authorization", "Bearer " + oAuthToken.getAccess_token());
+//		conn2.setDoOutput(true);
+//		BufferedReader br2 = new BufferedReader(new InputStreamReader(conn2.getInputStream(), "UTF-8"));
+//		String input2 = "";
+//		StringBuilder sb2 = new StringBuilder();
+//		while ((input2 = br2.readLine()) != null) {
+//			sb2.append(input2);
+//			System.out.println(input2);
+//		}
+//		
+//		Gson gson2 = new Gson();
+//		KakaoProfile kakaoProfile = gson2.fromJson(sb2.toString(), KakaoProfile.class);
+//		KakaoAccount ac = kakaoProfile.getAccount();
+//		//Profile pf = ac.getProfile();
+//		Profile pf = ac.getProfile(); // 객체를 가져오는 캐스팅이 안된다? 
+//		// 강제형변환을 강제로하면 작동은 함
+//		HashMap<String, Object> paramMap = new HashMap<String, Object>();
+//		paramMap.put("id", kakaoProfile.getId() );
+//		paramMap.put("ref_cursor", null );
+//		ms.getMember( paramMap );
+//		ArrayList< HashMap<String, Object> > list 
+//				= (ArrayList< HashMap<String, Object> >) paramMap.get("ref_cursor" );
+//		if ( list == null || list.size() == 0 ) {
+//			paramMap.put("id", kakaoProfile.getId() );
+//			paramMap.put("email" , ac.getEmail());
+//			paramMap.put("name" , ((MemberVO) pf).getNickname());
+//			paramMap.put("provider" , "kakao");
+//			ms.joinKakao( paramMap );
+//			
+//			paramMap.put("ref_cursor", null );
+//			ms.getMember( paramMap );
+//			list = (ArrayList< HashMap<String, Object> >) paramMap.get("ref_cursor" );
+//		}
+//		HashMap<String , Object> mvo = list.get(0);
+//		HttpSession session = request.getSession();
+//		session.setAttribute("loginUser", mvo);
+//		return "redirect:/";
+//	}
+//	
+	
+	
 	
 	
 	@RequestMapping("/kakaoLogin")
@@ -132,7 +192,7 @@ public class MemberController {
 		String endpoint = "https://kauth.kakao.com/oauth/token";
 		URL url = new URL(endpoint); // import java.net.URL;
 		String bodyData = "grant_type=authorization_code&";
-		bodyData += "client_id=4fde1cbde4d82ed7b25bd2a750d98d3c&";
+		bodyData += "client_id=bab305fbb8a02d4bd91ccc64d508aabe&";
 		bodyData += "redirect_uri=http://localhost:8070/kakaoLogin&";
 		bodyData += "code=" + code;
 		
@@ -169,19 +229,20 @@ public class MemberController {
 		Gson gson2 = new Gson();
 		KakaoProfile kakaoProfile = gson2.fromJson(sb2.toString(), KakaoProfile.class);
 		KakaoAccount ac = kakaoProfile.getAccount();
-		//Profile pf = ac.getProfile();
-		Profile pf = (Profile) ac.getProfile(); // 객체를 가져오는 캐스팅이 안된다? 
-		// 강제형변환을 강제로하면 작동은 함
+		com.ticket.t1.dto.KakaoProfile.KakaoAccount.Profile pf = ac.getProfile();
+		
+				
+		
 		HashMap<String, Object> paramMap = new HashMap<String, Object>();
 		paramMap.put("id", kakaoProfile.getId() );
 		paramMap.put("ref_cursor", null );
 		ms.getMember( paramMap );
-		ArrayList< HashMap<String, Object> > list 
-				= (ArrayList< HashMap<String, Object> >) paramMap.get("ref_cursor" );
+		ArrayList< HashMap<String, Object> > list = (ArrayList< HashMap<String, Object> >) paramMap.get("ref_cursor" );
 		if ( list == null || list.size() == 0 ) {
+			
 			paramMap.put("id", kakaoProfile.getId() );
 			paramMap.put("email" , ac.getEmail());
-			paramMap.put("name" , ((MemberVO) pf).getNickname());
+			paramMap.put("name" , pf.getNickname());
 			paramMap.put("provider" , "kakao");
 			ms.joinKakao( paramMap );
 			
@@ -194,6 +255,7 @@ public class MemberController {
 		session.setAttribute("loginUser", mvo);
 		return "redirect:/";
 	}
+	
 	
 
 	
